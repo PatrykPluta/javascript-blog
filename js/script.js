1971c05a265a52c1c6deddcd69b25fd32aac6d1e
Patryk Pluta <patryk_pluta@interia.pl>
2022-01-03 19:42:48 +0100

    usuniecie article

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 'use strict';
 
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event)
     /* remove class 'active' from all article links  */
-  
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
     /* add class 'active' to the clicked link */
-  
+
+    console.log('clickedElement (with plus): ' + clickedElement);
+
     /* remove class 'active' from all articles */
-  
+    const activeArticles = document.querySelectorAll('.posts article.active');
+    
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
     /* get 'href' attribute from the clicked link */
   
     /* find the correct article using the selector (value of 'href' attribute) */
