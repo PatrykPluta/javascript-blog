52960e90326f4c87e55d922d2bf378f59f79d931
Patryk Pluta <patryk_pluta@interia.pl>
2022-01-16 18:04:19 +0100

    błedy same błedy

--- js/script.js
+++ js/script.js
@@ -4,8 +4,9 @@
   const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles',
-    optArticleTagsSelector = '.post-tags .list';
-    optArticleAuthorSelector = '.post-author .author-name';
+    optArticleTagsSelector = '.post-tags .list',
+    optArticleAuthorSelector = '.post-author .author-name',
+    optTagsListSelector = '.tags.list';
 
   const titleClickHandler = function(event){
     event.preventDefault();
@@ -91,7 +92,24 @@
   }
   generateTitleLinks();
 
+  function calculateTagsParams() {
+    const params = {'max': 0, "min": 999999};
+    for(let tag in tags){
+      console.log(tag + ' is used ' + tags[tag] + ' times');
+      if(tags[tag] > params.max){
+        params.max = tags[tag];
+      } if(tags[tag] < params.min){
+        params.min = tags[tag];
+      }
+
+    }
+
+    return params;
+  }
+
   function generateTags() {
+      /* [NEW] create a new variable allTags with an empty object */
+      let allTags = {};
     /* find all articles */
     const articles  = document.querySelectorAll(optArticleSelector);
     /* START LOOP: for every article: */
@@ -113,12 +131,36 @@
         const linkHTML = '<li><a href="#tag-' + tag + '"><span>' + tag + '</span></a></li>';
         /* add generated code to html variable */
         html = html + linkHTML;
+        /* [NEW] check if this link is NOT already in allTags */
+        if(!allTags[tag]) {
+        /* [NEW] add tag to allTags object */
+        allTags[tag] = 1;
+       } else {
+        allTags[tag]++;
+      }
       /* END LOOP: for each tag */
       }
       /* insert HTML of all the links into the tags wrapper */
       tagsList.innerHTML = html;
     /* END LOOP: for every article: */
     }
+      /* [NEW] find list of tags in right column */
+      const tagList = document.querySelector(optTagsListSelector);
+
+    /* [NEW] create variable for all links HTML code */
+    const tagsParams = calculateTagsParams(allTags);
+    console.log('tagsParams:', tagsParams)
+    let allTagsHTML = '';
+
+    /* [NEW] START LOOP: for each tag in allTags: */
+    for(let tag in allTags){
+      /* [NEW] generate code of a link and add it to allTagsHTML */
+      allTagsHTML += tag + ' (' + allTags[tag] + ') ';
+    }
+    /* [NEW] END LOOP: for each tag in allTags: */
+
+    /*[NEW] add HTML from allTagsHTML to tagList */
+    tagList.innerHTML = allTagsHTML;
   };
 
   generateTags();
@@ -143,7 +185,7 @@
     /* find all tag links with "href" attribute equal to the "href" constant */
     const tagHref = document.querySelectorAll('a[href="' + href + '"]')
     /* START LOOP: for each found tag link */
-    for(let link of links){
+    for(let link of tagHref){
       /* add class active */
       link.classList.add('active');
     /* END LOOP: for each found tag link */
